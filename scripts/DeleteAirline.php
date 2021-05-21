34cc2412a91af96e3c475156aacc615b8ab65dbf
Zoe Kim 김민정 <47043866+zoekimm@users.noreply.github.com>
2021-05-21 14:38:02 +0900

    docs : removed comments

--- scripts/DeleteAirline.php
+++ scripts/DeleteAirline.php
@@ -6,20 +6,16 @@ $airlineCodeInput = $_POST["airlineCodeInput"];
 
 $sql = "DELETE FROM Airline WHERE airlineCode = ?;";
 
-//Determine if any input was actually collected
 if (empty($airlineCodeInput)) {
     
     echo "ERROR: empty mandatory input.";
 
 } else {
- 
-    //Prepare a statement that we can later execute. The ?'s are placeholders for
-    //parameters whose values we will set before we run the query.
+
     if ($stmt = $conn->prepare($sql)) {
  
        $stmt->bind_param("s", $airlineCodeInput);
 
-       //Run the actual query
         if ($stmt->execute()) {
             if ($stmt->affected_rows != 0) {
                 echo "airline code ".$airlineCodeInput." has been succeessfully deleted!<br>";
@@ -35,14 +31,11 @@ if (empty($airlineCodeInput)) {
         echo "<br>Execute failed. Check your inputs again.<br>";
 
        }
-
-       //Close down the prepared statement
+        
        $stmt->close();
  
     } else {
  
-        //A problem occurred when preparing the statement; check for syntax errors
-        //and misspelled attribute names in the statement string.
        echo "Prepare failed.<br>";
        $error = $conn->errno . ' ' . $conn->error;
        echo $error; 
@@ -50,6 +43,5 @@ if (empty($airlineCodeInput)) {
  
 }
 
- //Close the connection created in open.php
  $conn->close();
 ?>

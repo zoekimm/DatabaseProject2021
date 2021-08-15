5e66e224d287acdec255cc13b9790878c61da691
김민정 <zoek2100@gmail.com>
2021-08-15 13:53:33 +0900

    feat : display a data table of countries by GDP

---
+++ scripts/CountryByGDP.php
@@ -0,0 +1,58 @@
+<?php
+
+include 'open.php';
+$countryNameInput = $_POST["countryNameInput"];
+$dataPoints = array();
+
+if (empty($countryNameInput)) {
+    
+    echo "ERROR: emtpy input";
+
+} else {
+ 
+    if ($stmt = $conn->prepare("CALL CountryByGDP(?)")) {
+ 
+       $stmt->bind_param("s", $countryNameInput);
+ 
+       if ($stmt->execute()) {
+
+          $result = $stmt->get_result();
+ 
+          if ($result->num_rows == 0) {
+ 
+             echo "No result; Please check your input value again";
+
+          } else {
+      
+             echo "<table border=\"1px solid black\">";
+             echo "<tr><th> GDP </th></tr>";
+
+             while ($row = $result->fetch_row()) {
+                echo "<tr>";
+                echo "<td>".$row[0]."</td>";
+                echo "</tr>";
+                array_push($dataPoints, array("y" => $row[1], "label" => $row[0]));
+             } 
+             echo "</table>";
+          }	 
+ 
+          $result->free_result();
+       
+       } else {
+ 
+          echo "Execute failed.<br>";
+       }
+ 
+       $stmt->close();
+ 
+    } else {
+
+       echo "Prepare failed.<br>";
+       $error = $conn->errno . ' ' . $conn->error;
+       echo $error; 
+    }
+ 
+ }
+ 
+ $conn->close();
+?>
